7ca747aa50947fb1c993be2a74d0ee6d6a5cdb6c
jdlrobson <jdlrobson@gmail.com>
2020-12-20 09:52:11 -0800

    Add preview component

---
+++ src/components/Preview.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="preview">
+        <h3>Preview</h3>
+        <select v-if="enabled" @change="changeArticle">
+            <option v-for="(a,i) in articles" :key="i" :value="a.title">{{ a.name }}</option>
+          </select>
+        <div v-if="enabled" class="preview-area">
+          <iframe :src="demoLink" width="640" height="480" />
+        </div>
+        <div v-else class="preview__area preview__area--unavailable">
+          Preview unavailable.
+        </div>
+    </div>
+</template>
+
+<script>
+import { HOST, TEST_ARTICLE } from '../constants';
+
+export default {
+  name: 'Preview',
+  components: {},
+  props: {
+    enabled: {
+      type: Boolean
+    }
+  },
+  data() {
+      return {
+          articles: [
+            { title: TEST_ARTICLE, name: TEST_ARTICLE.replace(/_/g, ' ') },
+            { title: 'Rube Goldberg machine', name: 'Rube Goldberg machine' }
+          ],
+          testArticle: TEST_ARTICLE
+      };
+  },
+  computed: {
+    demoLink() {
+      return `${HOST}/wiki/${this.testArticle}?useskin=${this.$route.params.key}`;
+    }
+  },
+  methods: {
+    changeArticle: function ( ev ) {
+      this.testArticle = ev.target.value;
+    }
+  },
+};
+</script>
+
+<style scoped>
+    .preview__area {
+    overflow: hidden;
+  }
+  iframe {
+    background: white;
+    transform: scale(0.5, 0.5);
+    transform-origin: 0 0;
+    width: calc( 640px / 0.5 );
+    height: calc( 480px / 0.5 );
+  }
+  .preview__area--unavailable {
+    opacity: 0.5;
+    background: gray;
+    width: 640px;
+    height: 480px;
+    position: relative;
+    padding-top: 260px;
+  }
+  .preview__area--unavailable:after {
+    position: absolute;
+    top: calc( 220px - 50px);
+    bottom: 0;
+    left: 0;
+    right: 0;
+    content: "\274c";
+    font-size: 50px;
+    color: #FFF;
+    line-height: 100px;
+    text-align: center;
+  }
+</style>
--- src/pages/Skin.vue
+++ src/pages/Skin.vue
@@ -19,40 +19,27 @@
           :href="link.href">{{ link.text}}</a>
       </template>
       <template v-slot:column-two>
-        <h3>Preview</h3>
-        <select v-if="stable && preview" @change="changeArticle">
-            <option v-for="(a,i) in articles" :key="i" :value="a.title">{{ a.name }}</option>
-          </select>
-        <div v-if="stable && preview" class="preview-area">
-          <iframe :src="demoLink" width="640" height="480" />
-        </div>
-        <div v-else class="preview-area preview-area--unavailable">
-          Preview unavailable.
-        </div>
+        <preview :enabled="stable && preview"></preview>
       </template>
     </two-column-layout>
   </div>
 </template>
 
 <script>
-import { HOST, TEST_ARTICLE } from '../constants';
 import api from '../api.js';
 import TwoColumnLayout from '../components/TwoColumnLayout';
 import Snapshot from '../components/Snapshot.vue';
+import Preview from '../components/Preview.vue';
 
 export default {
   name: 'Skin',
   components: {
       Snapshot,
+      Preview,
       TwoColumnLayout
   },
   data() {
       return {
-          articles: [
-            { title: TEST_ARTICLE, name: TEST_ARTICLE.replace(/_/g, ' ') },
-            { title: 'Rube Goldberg machine', name: 'Rube Goldberg machine' }
-          ],
-          testArticle: TEST_ARTICLE,
           stable: true,
           preview: true,
           name: this.$route.params.key.replace( /[^⠀]/g, '⠀' ) + '⠀',
@@ -64,14 +51,6 @@ export default {
   computed: {
     mwUrl() {
       return this.name ? `https://mediawiki.org/wiki/Skin:${this.name}` : '';
-    },
-    demoLink() {
-      return `${HOST}/wiki/${this.testArticle}?useskin=${this.$route.params.key}`;
-    }
-  },
-  methods: {
-    changeArticle: function ( ev ) {
-      this.testArticle = ev.target.value;
     }
   },
   mounted: function() {
@@ -134,53 +113,7 @@ export default {
     border: dashed 3px orange;
     margin-bottom: 20px;
   }
-  article {
-    display: flex;
-    justify-content: space-between;
-    flex-direction: row;
-    flex-flow: row wrap;
-  }
-  aside {
-    width: 360px;
-  }
-  aside > * {
-    margin-left: auto;
-    margin-right: auto;
-  }
-  aside + aside {
-    width: 640px;
-  }
-  .preview-area {
-    overflow: hidden;
-  }
-  iframe {
-    background: white;
-    transform: scale(0.5, 0.5);
-    transform-origin: 0 0;
-    width: calc( 640px / 0.5 );
-    height: calc( 480px / 0.5 );
-  }
-  .preview-area--unavailable {
-    opacity: 0.5;
-    background: gray;
-    width: 640px;
-    height: 480px;
-    position: relative;
-    padding-top: 260px;
-  }
-  .preview-area--unavailable:after {
-    position: absolute;
-    top: calc( 220px - 50px);
-    bottom: 0;
-    left: 0;
-    right: 0;
-    content: "\274c";
-    font-size: 50px;
-    color: #FFF;
-    line-height: 100px;
-    text-align: center;
-  }
-  aside p {
+  p {
     width: 320px;
   }
   .skinLink {
