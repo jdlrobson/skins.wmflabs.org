6ce882de95d51ab80a4cf6ce073db6710cf7cbae
jdlrobson <jdlrobson@gmail.com>
2020-12-20 21:54:54 -0800

    Shrink screenshots and expose category for unmaintained skins

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,7 @@ export const TEST_ARTICLES = [
 ];
 export const CATEGORY_SKINS = 'Category:Stable_skins';
 export const CATEGORY_BETA_SKINS = 'Category:Beta_status_skins';
+export const CATEGORY_UNMAINTAINED_SKINS = 'Category:Unmaintained_skins';
 
 export const HIDDEN_SKINS = [
     // advertising skins
@@ -29,7 +30,7 @@ export const SCREENSHOTS = {
     cavendish: 'https://upload.wikimedia.org/wikipedia/mediawiki/thumb/1/13/Cavendish-Skin.jpg/440px-Cavendish-Skin.jpg',
     aether: 'https://upload.wikimedia.org/wikipedia/commons/9/97/Mediawiki-skin-aether.png',
     anisa: 'https://upload.wikimedia.org/wikipedia/commons/e/e7/Anisa_screenshot.png',
-    athena: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Athena_skin_1.png/700px-Athena_skin_1.png',
+    athena: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Athena_skin_1.png/440px-Athena_skin_1.png',
     erudite: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Erudite_skin.png/440px-Erudite_skin.png',
     webplatform: 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Screen_of_MediaWiki_skin_WebPlatform.png',
     woogleshades: 'https://upload.wikimedia.org/wikipedia/commons/1/10/WoOgLeShades.png',
@@ -37,10 +38,10 @@ export const SCREENSHOTS = {
     darkcosmos: 'https://upload.wikimedia.org/wikipedia/commons/8/83/Perfect_dark_theme_design.jpg',
     femiwiki: 'https://upload.wikimedia.org/wikipedia/commons/8/8a/Skin-Femiwiki.png',
     liberty: 'https://upload.wikimedia.org/wikipedia/commons/6/66/Screenshot-skin-liberty.png',
-    citizen: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Screen_Shot_of_MediaWiki_Citizen_skin.png/1024px-Screen_Shot_of_MediaWiki_Citizen_skin.png',
+    citizen: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Screen_Shot_of_MediaWiki_Citizen_skin.png/480px-Screen_Shot_of_MediaWiki_Citizen_skin.png',
     refreshed: 'https://upload.wikimedia.org/wikipedia/commons/6/62/Refreshed4Big.png',
     dusktodawn: 'https://upload.wikimedia.org/wikipedia/commons/d/d7/DuskToDawn.png',
-    medik: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Skin_Medik.png/2560px-Skin_Medik.png',
+    medik: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Skin_Medik.png/480px-Skin_Medik.png',
     darkvector: 'https://upload.wikimedia.org/wikipedia/commons/8/84/DarkVector.png',
     gamepress: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Gamepress_Skin_Screenshot.png/700px-Gamepress_Skin_Screenshot.png',
     bluespicecalumma: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Screen_Shot_of_Blue_Spice_Calumma_Skin.png/600px-Screen_Shot_of_Blue_Spice_Calumma_Skin.png',
