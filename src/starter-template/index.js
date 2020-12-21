e01a6969d09e50bdee09bca517c33982fcf98865
jdlrobson <jdlrobson@gmail.com>
2020-12-21 15:04:16 -0800

    Update the starter template

--- src/starter-template/index.js
+++ src/starter-template/index.js
@@ -2,23 +2,35 @@ import fs from 'fs';
 
 const FooterList = fs.readFileSync(`${__dirname}/FooterList.mustache`).toString();
 const Portlet = fs.readFileSync(`${__dirname}/Portlet.mustache`).toString();
-const Indicators = fs.readFileSync(`${__dirname}/Indicators.mustache`).toString();
+const ContentIndicators = fs.readFileSync(`${__dirname}/ContentIndicators.mustache`).toString();
 const Notices = fs.readFileSync(`${__dirname}/Notices.mustache`).toString();
-const ArticleHeader = fs.readFileSync(`${__dirname}/ArticleHeader.mustache`).toString();
+const ContentHeading = fs.readFileSync(`${__dirname}/ContentHeading.mustache`).toString();
+const ContentActions = fs.readFileSync(`${__dirname}/ContentActions.mustache`).toString();
+const ContentNamespaces = fs.readFileSync(`${__dirname}/ContentNamespaces.mustache`).toString();
+const ContentBody = fs.readFileSync(`${__dirname}/ContentBody.mustache`).toString();
+const ContentTagline = fs.readFileSync(`${__dirname}/ContentTagline.mustache`).toString();
 const Footer = fs.readFileSync(`${__dirname}/Footer.mustache`).toString();
 const Logo = fs.readFileSync(`${__dirname}/Logo.mustache`).toString();
 const Search = fs.readFileSync(`${__dirname}/Search.mustache`).toString();
 const Sidebar = fs.readFileSync(`${__dirname}/Sidebar.mustache`).toString();
+const PersonalMenu = fs.readFileSync(`${__dirname}/PersonalMenu.mustache`).toString();
+const Languages = fs.readFileSync(`${__dirname}/Languages.mustache`).toString();
 
 export const PARTIALS = {
+    Languages,
+    PersonalMenu,
     Footer,
     Logo,
     Search,
     Sidebar,
     FooterList,
-    ArticleHeader,
+    ContentHeading,
+    ContentActions,
+    ContentNamespaces,
+    ContentTagline,
+    ContentBody,
     Notices,
-    Indicators,
+    ContentIndicators,
     Portlet
 };
 
