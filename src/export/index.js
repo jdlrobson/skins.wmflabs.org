819feb2c48da7cef1fc98ff9fa2e1b4eb9385f6b
jdlrobson <jdlrobson@gmail.com>
2021-05-19 15:56:22 -0700

    Also save an eslint configuration

--- src/export/index.js
+++ src/export/index.js
@@ -3,6 +3,7 @@ import saveAs from './FileSaver';
 import { DEFAULT_FEATURES, SKINS_LAB_VERSION, MW_MIN_VERSION } from '../starter-template/index';
 const TOOL_LINK = `[https://skins.wmflabs.org skins.wmflabs.org v.${SKINS_LAB_VERSION}]`;
 import packageJSON from '../starter-template/_package.json';
+import eslintJSON from '../starter-template/_eslintrc.json';
 
 function stringifyjson(json) {
     return JSON.stringify(json, null, 2);
@@ -142,6 +143,7 @@ function build(name, styles, templates, scripts = {}, messages = []) {
         )
     );
     rootfolder.file('package.json', stringifyjson( packageJSON ) );
+    rootfolder.file('.eslintrc.json', stringifyjson( eslintJSON ) );
     rootfolder.file('.gitignore', `.eslintcache
 node_modules/
 `);
