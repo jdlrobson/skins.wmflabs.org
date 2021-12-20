b728bcff609069454681ce3a4de29be6f12f48ec
jdlrobson <jdlrobson@gmail.com>
2021-12-20 12:26:09 -0800

    Clarify why unstable skins are missing

--- src/components/Searcher.vue
+++ src/components/Searcher.vue
@@ -67,6 +67,8 @@
 				😞
 			</div>
 			<p>No skins match that name.</p>
+			<p>If the skin exists it may not be compatible with the latest MediaWiki version
+				and be <a href="https://www.mediawiki.org/wiki/Category:Unstable_skins">marked as unstable</a>.</p>
 			<p>You can <a :href="mwUrlEdit">add this skin to MediaWiki.org</a></p>
 		</div>
 	</div>
