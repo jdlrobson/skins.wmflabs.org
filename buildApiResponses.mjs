a7b3db4393bf16743d9eb8aa7f05bc41e73c6322
Jon Robson <jdlrobson@gmail.com>
2023-03-31 15:43:15 -0700

    Report where screenshots are missing

--- buildApiResponses.mjs
+++ buildApiResponses.mjs
@@ -7,6 +7,9 @@ global.fetch = fetch;
 api.fetchSkinsRemote().then( ( json ) => {
 	if ( json.skins ) {
 		json.modified = new Date();
+		const skinsWithoutImages = json.skins.filter((x) => !x.src).map((x) => x.name);
+		console.log( `The following skins lack screenshots:
+${skinsWithoutImages.map((s)=>`wfLoadSkin('${s}');`).join('\n')}`);
 		fs.writeFileSync( 'data/skins.json', JSON.stringify( json ) );
 	} else {
 		console.error( 'response in unknown form' );
